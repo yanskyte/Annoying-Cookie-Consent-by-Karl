b9812cce177dc03eda1ae34c2fc9045be892eb01
Karl Ian Palisoc <krln.plsc@gmail.com>
2025-03-18 12:04:16 +0800

    commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
 const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -13,19 +14,8 @@ modalCloseBtn.addEventListener('click', function(){
 })
 
 declineBtn.addEventListener('mouseenter', function(){
-    console.log('hovered')
+    modalChoiceBtns.classList.toggle('modal-btns-reverse')
 })
-
-/*   
-Challenge: 
-1. Take control of the div holding the buttons.
-2. In index.css, set up a selector for a new class
-   that uses flex-direction to reverse the order 
-   of its child elements.
-3. Toggle that class on the div holding the buttons 
-   when a user's cursor hovers over the decline button. 
-*/ 
-
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
